2f585b43251adcd9ba4b3cf935ab61ce84019c59
BLTDDHC <bouakoleader@icloud.com>
2026-09-24 12:57:58 +0100

    fix: make regime optional outside primary cycles

--- lib/data/services/store_service.dart
+++ lib/data/services/store_service.dart
@@ -2000,7 +2000,7 @@ class StoreService extends ChangeNotifier {
           .toList();
 
   Future<void> changeStudentClassRemote(String studentId, String classId,
-      {String schoolRegime = 'normal',
+      {String? schoolRegime,
       bool hasTd = false,
       Map<String, dynamic> registrationOptions = const {}}) async {
     final history = await _repository.studentRegistrations(studentId);
@@ -2768,7 +2768,7 @@ class StoreService extends ChangeNotifier {
 
   Future<Map<String, dynamic>> approvePreEnrollmentRemote(String id,
           {String? classId,
-          String schoolRegime = 'normal',
+          String? schoolRegime,
           bool hasTd = false,
           Map<String, dynamic> options = const {}}) =>
       _repository.approvePreEnrollment(
