7a340f5b378143b769c284dd2b81ae34451a9b41
BLTDDHC <bouakoleader@icloud.com>
2026-09-24 10:16:15 +0100

    fix: let approval reuse persisted pre-enrollment regime

--- lib/data/repositories/school_repository.dart
+++ lib/data/repositories/school_repository.dart
@@ -412,13 +412,13 @@ class SchoolRepository {
 
   Future<Map<String, dynamic>> createStudentRegistration(
           String studentId, String classId,
-          {String schoolRegime = 'normal',
+          {String? schoolRegime,
           bool hasTd = false,
           Map<String, dynamic> options = const {}}) async =>
       Map<String, dynamic>.from(
           await _api.post('/api/v1/school/students/$studentId/registrations', {
         'classId': classId,
-        'schoolRegime': schoolRegime,
+        if (schoolRegime != null) 'schoolRegime': schoolRegime,
         'hasTd': hasTd,
         'options': options,
       }));
